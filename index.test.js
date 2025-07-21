572cf16b0bf9e6da00a7047c84382e2be304a2ec
ValeriEnchev <valee70@mail.bg>
2025-07-22 00:10:04 +0300

    test commit

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 const sum = require('./index');
 
-test('add 1 plus 2 to equal', () => {
-    expect(sum(1,2)).toBe(3)
+test('add 1 plus 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(13)
 
 });
